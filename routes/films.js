26485d63f1240a064b5dd8329ef345f665f3e6a3
Vadithalion <al375550@uji.es>
2020-04-23 10:09:14 +0200

    Reworking

--- routes/films.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const filmsController = require('../controllers/filmsController.js');
-
-router.get('/',filmsController.getAll);
-
-
-module.exports = router;
